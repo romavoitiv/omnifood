c50c9f0d35c462c848ecfeae9c3a22ffd959e0e5
romavoitiv <roma.voytiv@gmail.com>
2023-07-22 17:24:00 +0300

    try 6

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ const headerEl = document.querySelector('.header');
 const year = document.querySelector('.year');
 year.textContent = new Date().getFullYear();
 
-function preventDefault(e){
-    e.preventDefault();
-}
-function disableMobileScroll() {
-    document.body.addEventListener('touchmove', preventDefault, { passive: false });
-}
-function enableMobileScroll() {
-    document.body.removeEventListener('touchmove', preventDefault, { passive: false });
-}
+
+// function disableMobileScroll() {
+    
+// }
+// function enableMobileScroll() {
+//     document.body.removeEventListener('touchmove', preventDefault, { passive: false });
+// }
+
+
 
 // Handle burger (nav-open class)
 const htmlEl = document.body.parentElement;
@@ -22,8 +22,16 @@ const navBtn = document.querySelector('.header__nav-btn');
 navBtn.addEventListener('click', function () {
   htmlEl.classList.toggle('nav-open');
 
-  htmlEl.classList.contains('nav-open') ? disableMobileScroll() : enableMobileScroll();
+  // htmlEl.classList.contains('nav-open') ? disableMobileScroll() : enableMobileScroll();
 });
+
+document.body.addEventListener('touchmove', preventDefault, { passive: false });
+
+function preventDefault(e) {
+  if (htmlEl.classList.contains('nav-open')) {
+    e.preventDefault();
+  }
+}
 
 // handle smooth scrolling
 const allLinks = document.querySelectorAll('a:link');
@@ -47,7 +55,7 @@ allLinks.forEach((link) => {
 
     if (link.classList.contains('header__nav-link')) {
       htmlEl.classList.remove('nav-open');
-      enableMobileScroll();
+      // enableMobileScroll();
     }
   })
 })
